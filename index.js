0eafe628f06d5aa3fe3c43f2a703fcfd68e78c8d
f-rahim <freelancerrahim26@gmail.com>
2023-06-06 12:14:17 +0600

    Added user API

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// all datas
+const users = require("./data/user.json");
+
 app.get("/", (req, res) => {
   res.send("Simple MongoDB Server is running");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
 
 app.listen(port, () => {
